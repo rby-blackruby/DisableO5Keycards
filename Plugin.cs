e1102e5d348d3cee86b84f39cbb1e49aacbd90a8
blackruby <58123929+rby-blackruby@users.noreply.github.com>
2020-02-09 13:01:19 +0100

    First version of DisableO5Keycards.

---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using Harmony;
+
+
+namespace DisableO5Keycards
+{
+    public class Plugin : EXILED.Plugin
+    {
+        public override string getName { get; } = "DisableO5 - blackruby";
+        public static bool DisableO5;
+        public static uint PatchCount;
+
+        public static HarmonyInstance HarmonyInstance { set; get; }
+
+        public override void OnEnable()
+        {
+            DisableO5 = Plugin.Config.GetBool("disable_o5", true);
+
+            if(DisableO5 == false)
+            {
+                Plugin.Info("DisableO5 is disabled on this server.");
+                return;
+            }
+
+            Plugin.Info("Thank you for using DisableO5 - blackruby");
+            Plugin.Info("While disable_o5 is set to true, nobody is able to upgrade their keycards to O5 on the server.");
+
+            HarmonyInstance = HarmonyInstance.Create($"blackruby.exiled.disableo5{PatchCount}");
+            PatchCount++;
+            HarmonyInstance.PatchAll();
+        }
+
+        public override void OnDisable()
+        {
+            HarmonyInstance.UnpatchAll();
+        }
+
+        public override void OnReload()
+        {
+            
+        }
+
+    }
+}
---
+++ YouCantUpgradeToO5haha.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using System;
+
+namespace DisableO5Keycards
+{
+    [HarmonyPatch(typeof(Scp914.Scp914Machine), "UpgradeItem", new Type[] { typeof(Pickup) } )]
+    class YouCantUpgradeToO5haha
+    {
+
+        public static bool Prefix(Scp914.Scp914Machine __instance, Pickup item)
+        {
+            ItemType itemType = __instance.UpgradeItemID(item.info.itemId);
+            if (itemType == ItemType.KeycardO5)
+            {
+                return false;
+            }
+            if (itemType < ItemType.KeycardJanitor)
+            {
+                item.Delete();
+                return false;
+            }
+            item.SetIDFull(itemType);
+            return true;
+
+        }
+    }
+}
